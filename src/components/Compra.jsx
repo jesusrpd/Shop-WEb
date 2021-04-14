c31358f89441d06b2b2483490cf5aa0bd7800b8a
jesusrpd <jesusrodrigo881@gmail.com>
2021-04-14 15:46:07 -0500

    Correccion de errores y responsive finalizado

--- src/components/Compra.jsx
+++ src/components/Compra.jsx
@@ -5,14 +5,16 @@ const Compra = ({nombre, precio, departamento, img, id, onClick, confirm})=>{
     return(
         <div className="carrito-card">
             <img className="carrito-card-img" src={img} alt={nombre}/>
-            <div className="carrito-card-info">
-                <p>{nombre}</p>
-                <p>${precio}Mxn.</p>
-                <p>{departamento}</p>
-            </div>
-            <div>
-                <button onClick={()=> onClick(id)} className="carrito-card-button btn-orange">Eliminar</button>
-                <button onClick={confirm} className="carrito-card-button btn-primary">Comprar</button>
+            <div className="carrito-info">
+                <div className="carrito-card-info">
+                    <p>{nombre}</p>
+                    <p>${precio}Mxn.</p>
+                    <p>{departamento}</p>
+                </div>
+                <div>
+                    <button onClick={()=> onClick(id)} className="carrito-card-button btn-orange">Eliminar</button>
+                    <button onClick={confirm} className="carrito-card-button btn-primary">Comprar</button>
+                </div>
             </div>
         </div>
     );
